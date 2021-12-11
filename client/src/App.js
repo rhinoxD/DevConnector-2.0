c22577b0702835fc6561fbc47e93feaf866c253b
rhinoxD <coolshiv0721@gmail.com>
2021-12-11 10:47:22 +0530

    Landing page almost done

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Landing />
+        <Navbar />
+        <Landing />
     </Router>
   );
 }
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div className='landing'>
-    </div>
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Developer Connector</h1>
+          <p className='lead'>
+            Create a developer profile/portfolio, share posts and get help from
+            other developers
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn'>
+              <span className='span'></span>
+              <span className='span'></span>
+              <span className='span'></span>
+              <span className='span'></span>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn'>
+              <span className='span'></span>
+              <span className='span'></span>
+              <span className='span'></span>
+              <span className='span'></span>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
