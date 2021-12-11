05b6cd554ea2260a35f5b94ae8d735c77feb3add
rhinoxD <coolshiv0721@gmail.com>
2021-12-11 12:09:48 +0530

    Register and Login Route Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
 function App() {
   return (
     <Router>
-        <Navbar />
-        <Landing />
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <Route exact path='/register' component={Register} />
+      <Route exact path='/login' component={Login} />
     </Router>
   );
 }
