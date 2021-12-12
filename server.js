d84ed44c18428f6fa82bba68eb09a731b0cbc1da
rhinoxD <coolshiv0721@gmail.com>
2021-12-12 10:17:14 +0530

    MongoDB Connected

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
 app.get('/', (req, res) => res.send('API Running...'));
+
+// Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
