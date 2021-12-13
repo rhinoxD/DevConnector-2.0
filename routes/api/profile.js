233b7c06c504cd15091c3b231261726d07dde1eb
rhinoxD <coolshiv0721@gmail.com>
2021-12-13 10:00:28 +0530

    Delete posts, profile, and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -136,4 +136,25 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route  DELETE api/profile
+// @desc   Delete profile, user and posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
