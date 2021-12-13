a3d28c48603978602ad11410280dee7bb89f66e5
rhinoxD <coolshiv0721@gmail.com>
2021-12-13 18:21:07 +0530

    Create a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,38 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route  GET /api/posts
-// @desc   Get posts
-// @access Public  
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
-module.exports = router; 
+// @route  POST /api/posts
+// @desc   Create a post
+// @access Private
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+module.exports = router;
