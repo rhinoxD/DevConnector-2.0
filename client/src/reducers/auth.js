8b0d0dd7111f055fd5e1338057c7cd76357a07c6
rhinoxD <coolshiv0721@gmail.com>
2021-12-14 11:10:51 +0530

    Created auth reducer

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,32 @@
+import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null,
+};
+
+const auth = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case REGISTER_FAIL:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default auth;
