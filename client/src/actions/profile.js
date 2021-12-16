329668af646b36ae546ca6fbab61d8bf056a3aa2
rhinoxD <coolshiv0721@gmail.com>
2021-12-16 19:01:31 +0530

    Delete Experience Added in Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -118,3 +118,20 @@ export const addEducation =
       });
     }
   };
+
+// Delete Experience
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert('Experience Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
