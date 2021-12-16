5e4a881167644e96a6cb6f6f2a33e988daecd9f6
rhinoxD <coolshiv0721@gmail.com>
2021-12-16 14:32:12 +0530

    EditProfile Component Added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -22,7 +23,9 @@ const Dashboard = ({
           <i className='fas fa-user' /> Welcome {user && user.name}
         </p>
         {profile !== null ? (
-          <>has</>
+          <>
+            <DashboardActions />
+          </>
         ) : (
           <>
             <p>You have not yet setup a profile, please add some info</p>
