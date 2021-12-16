2999ac189afcde9b739d3b41e4facdd5f3339820
rhinoxD <coolshiv0721@gmail.com>
2021-12-16 09:26:26 +0530

    Private Route Created and Added to App

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -34,6 +36,7 @@ function App() {
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
           </Switch>
         </section>
       </Router>
