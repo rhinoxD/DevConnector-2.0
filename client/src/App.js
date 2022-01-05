21b52905c4933380c42ff5162dade5a6f8a39808
rhinoxD <coolshiv0721@gmail.com>
2022-01-05 18:19:48 +0530

    Added Post Component in App

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -85,6 +86,7 @@ function App() {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Router>
