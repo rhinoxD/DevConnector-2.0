ddc3cbbfb01e3a053edb5bfb6820d902b81565d5
rhinoxD <coolshiv0721@gmail.com>
2022-01-05 09:53:49 +0530

    Added Profiles Component in App

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -51,10 +52,7 @@ function App() {
     <ThemeProvider theme={themes[theme]}>
       <Provider store={store}>
         <Router>
-          <Navbar
-            theme={theme}
-            setTheme={setTheme}
-          />
+          <Navbar theme={theme} setTheme={setTheme} />
           <Switch>
             <Route exact path='/' component={Landing} />
           </Switch>
@@ -63,6 +61,7 @@ function App() {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
